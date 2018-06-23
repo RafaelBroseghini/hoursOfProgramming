45c41c3d90170fb62aef4bb4dcff52302cc85c71
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-06-23 13:41:36 -0500

    refactoring

--- app.js
+++ app.js
@@ -1,66 +1,25 @@
 var express    = require("express"),
     app        = express(),
     mongoose   = require("mongoose"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    Session = require("./models/session");
 
-app.use(express.static("public"));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+
+var sessionRoutes = require("./routes/sessions"),
+    indexRoutes   = require("./routes/index")
 
 mongoose.connect("mongodb://localhost/programming");
 
-var sessionSchema = new mongoose.Schema({
-    language: String,
-    framework: String,
-    hours: Number,
-    topic: String
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 
-var session = mongoose.model("session", sessionSchema);
-
-app.get("/", function(req, res) {
-    res.render("index");
-});
-
-app.post("/hours", function(req, res){
-    var l = req.body.language,
-        f = req.body.framework,
-        h = req.body.hours,
-        t = req.body.topic;
-
-    session.create({
-        language: l,
-        framework: f,
-        hours: h,
-        topic: t,
-    })
-
-    console.log(l + " " + f + " " + h + " " + t);
-    res.redirect("results")    
-});
-
-app.get("/results",function(req, res){    
-    session.find({}, function(err, sessions){
-        if (err) {
-            res.redirect("/")
-        } else {
-            res.render("results", {data: lanDict(sessions)});
-        }
-    })
-  })
-
-function lanDict(arr){
-    aggregate = {}
-    for (const lan of arr) {
-        if (!(lan.language in aggregate)) {
-            aggregate[lan.language] = lan.hours
-        } else {
-            aggregate[lan.language] += lan.hours
-        }
-    }
-    return aggregate
-}
+app.use(indexRoutes);
+app.use(sessionRoutes);
 
 app.listen(8000, function(){
     console.log("Hours of programming app!");
 });
+
+
+
